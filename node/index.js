415b4b1d962df5dfd84a6cdc7680260d546d6de4
Diego Xavier Pinheiro <96208261+diegoxp1994@users.noreply.github.com>
2024-09-12 14:33:21 -0300

    Update index.js

--- node/index.js
+++ node/index.js
@@ -10,14 +10,30 @@ const connection = mysql.createConnection({
     database: 'nodedb'
 })
 
-const sql = `INSERT INTO people(NAME) values('Diego')`
+const create_table = 'CREATE TABLE IF NOT EXISTS people (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;'
+connection.query(create_table)
+
+const sql = `INSERT INTO people(name) values ('Diego')`
 connection.query(sql)
+
+const select = 'SELECT name FROM people'
+let list = ''
+connection.query(select, (_err, result, _) => {
+  let i
+  for (i = 0; i < result.length; i++) {
+    list += '<li>' + result[i].name + '</li>'
+  }
+})
 connection.end()
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
+app.get('/', (_req, res) => {
+  let html = '<h1>Full Cycle Rocks!</h1></br>'
+  html += '<ul>'
+  html += list
+  html += '</ul>'
+  res.send(html)
 })
 
-app.listen(port, ()=> {
-    console.log('Rodando na porta: ' + port)
+app.listen(port, () => {
+  console.log('Rodando na porta ', port)
 })
